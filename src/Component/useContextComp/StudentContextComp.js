79dea848ea385040fc629b5ff7ab3763cff35d28
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2024-02-16 16:08:05 +0530

    useContext-Hook

--- src/Component/useContextComp/StudentContextComp.js
+++ src/Component/useContextComp/StudentContextComp.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import studentContext from "./studentContext";
 export default function StudentContextComp(props) {
-  const name2 = "Ranade";
-  const [name, setName] = useState("Amruta");
+  const name2 = "More";
+  const [name, setName] = useState("Ashwini");
   //const [students, setStudents] = useState([]);
   return (
     <studentContext.Provider value={{ name, name2 }}>
