052b7a6c635a2aef47d95abd24128f663eb3027b
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2024-02-16 11:57:48 +0530

    -Props

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Greet from './Component/FuncComp';
 import Welcome from './Component/ClassComp';
 import CompLife from './Component/CompLifeCycle';
 import Stateexample from './Component/StateObj';
+import Propsexample from './Component/Props';
 
 function App() {
+  const data={name:'Swami'};
   return (
     <div className="App">
       <header className="App-header">
@@ -29,6 +31,8 @@ function App() {
         <CompLife />
         <h1 style={{color: "#87CEEB"}}>State Object</h1>
         <Stateexample />
+        <h1 style={{color: "#87CEEB"}}>Props example</h1>
+        <Propsexample data={data} />
       </header>
       
     </div>
---
+++ src/Component/Props.js
@@ -0,0 +1,4 @@
+export default function Propsexample(props)
+{
+	return <h2>Hello {props.data.name}..</h2>
+}
